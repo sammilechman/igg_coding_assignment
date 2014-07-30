63d5a9ace97ee8c7a71a8b1e343a3681dcd894ff
Sam Milechman <sammilechman@gmail.com>
2014-07-30 09:18:57 -0700

    Working draft

--- rpn.rb
+++ rpn.rb
@@ -3,7 +3,7 @@
 def ctoi(char)
   return_i = char.ord - "0".ord
   if return_i < 0 || return_i > 9
-    puts "invalid number"
+    puts "Invalid number"
     exit
   end
   return_i
@@ -18,6 +18,7 @@ def atoi(str)
 end
 
 def atof(str)
+  return str if str.class == Fixnum || str.class == Float
   arr_of_str = str.split(/\D/)
   if arr_of_str.length == 2
     total = 0
@@ -35,53 +36,113 @@ def start
   calculate(array)
 end
 
-def calculate(arr)
-  finished_arr = []
+def calculate(arr, stack = [])
+  p arr
+  p stack
+  puts " "
   operators = ["+", "-", "*", "/"]
-  start_pos = 0
+  digits = ('0'..'9').to_a
 
-  arr.each_index do |idx|
-    if operators.include?(arr[idx])
-      to_perform = arr[start_pos..idx]
-
-      if to_perform.length < 3
-        to_perform = finished_arr << to_perform
-      end
-
-      finished_arr << perform_op(to_perform)
-      start_pos = idx + 1
-      p start_pos
-    end
-  end
-
-  if finished_arr.length == 1
-    finished_arr.first
-  else
-    arr = finished_arr << arr[start_pos..-1]
-    p arr
-    calculate(arr)
-  end
-end
-
-def perform_op(arr)
-  if arr.length < 3
-    puts "not enough arguments"
-    exit
-  elsif arr.length == 3
-    case arr.last
-    when "+"
-      return atof(arr[0]) + atof(arr[1])
-    when "-"
-      return atof(arr[0]) - atof(arr[1])
-    when "*"
-      return atof(arr[0]) * atof(arr[1])
-    when "/"
-      return atof(arr[0]) / atof(arr[1])
-    else
-      puts "invalid operator"
+  if arr.length == 3 && stack.empty?
+    p "yooo"
+    val2, val1, op = arr.shift, arr.shift, arr.shift
+    result = perform_op(op, val1, val2)
+    puts "The result of the calculation is: " + result
+    return
+  elsif arr.length <= 1 && !stack.empty?
+    p "end"
+    calculate(stack + arr, [])
+  elsif operators.include?(arr.first)
+    if stack.length < 2
+      puts "Not enough arguments."
       exit
+    else
+      val2, val1 = stack.shift, stack.shift
+      result = perform_op(arr.shift, val1, val2)
+      puts "result: " + result
+      stack.unshift(result)
+      calculate(arr, stack)
     end
+  elsif stack.length == 1 && arr.empty?
+    calculate(stack, arr)
+  else
+    stack.unshift(arr.shift)
+    calculate(arr, stack)
+  end
+
+end
+
+def perform_op(op, val1, val2)
+  case op
+  when "+"
+    return (atof(val1) + atof(val2)).to_s
+  when "-"
+    return (atof(val1) - atof(val2)).to_s
+  when "*"
+    return (atof(val1) * atof(val2)).to_s
+  when "/"
+    return (atof(val1) / atof(val2)).to_s
+  else
+    puts "Invalid operator."
+    exit
   end
 end
+
+
+
+
+
+
+
+
+
+# def calculate(arr)
+#   finished_arr = []
+#   operators = ["+", "-", "*", "/"]
+#   start_pos = 0
+#
+#   arr.each_index do |idx|
+#     if operators.include?(arr[idx])
+#       to_perform = arr[start_pos..idx]
+#
+#       if to_perform.length < 3
+#         to_perform = finished_arr << to_perform
+#       end
+#
+#       finished_arr << perform_op(to_perform)
+#       start_pos = idx + 1
+#       p start_pos
+#     end
+#   end
+#
+#   if finished_arr.length == 1
+#     finished_arr.first
+#   else
+#     arr = finished_arr << arr[start_pos..-1]
+#     p arr
+#     calculate(arr)
+#   end
+# end
+#
+# def perform_op(arr)
+#   if arr.length < 3
+#     puts "not enough arguments"
+#     exit
+#   elsif arr.length == 3
+#     case arr.last
+#     when "+"
+#       return atof(arr[0]) + atof(arr[1])
+#     when "-"
+#       return atof(arr[0]) - atof(arr[1])
+#     when "*"
+#       return atof(arr[0]) * atof(arr[1])
+#     when "/"
+#       return atof(arr[0]) / atof(arr[1])
+#     else
+#       puts "invalid operator"
+#       exit
+#     end
+#   end
+# end
 
 start
