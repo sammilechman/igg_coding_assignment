68515544f6df87ff24755f67deadb848306d68f2
Sam Milechman <sammilechman@gmail.com>
2014-07-30 15:51:27 -0700

    Cleanup code round 1

--- rpn.rb
+++ rpn.rb
@@ -3,8 +3,7 @@
 def ctoi(char)
   return_i = char.ord - "0".ord
   if return_i < 0 || return_i > 9
-    p char
-    puts "Invalid number"
+    puts "Invalid number."
     exit
   end
   return_i
@@ -28,15 +27,36 @@ def atof(str)
   elsif arr_of_str.length == 1
     1.0 * atoi(str)
   else
-    puts "This is not a valid float."
+    puts "Invalid number."
+    exit
   end
 end
 
 def ftoi(str)
-  #Simplifies result from float to int if possible.
-  arr = str.split(".")
-  return atoi(arr[0]).to_s if arr[1] == "0"
-  arr.join(".")
+  #Simplifies result from float to integer if possible.
+  #First, sanitize negative sign.
+  if str[0] == "-"
+    negative = true
+    str[0] = ""
+  end
+
+  num = integer_check_stringify(str.split("."))
+
+  #Return negative sign if applicable.
+  if negative
+     "-" + num
+  else
+     num
+  end
+end
+
+def integer_check_stringify(arr)
+  #If it only has 0 after decimal, it's an integer.
+  if arr[1] == "0"
+    arr[0].to_s
+  else
+    arr.join(".")
+  end
 end
 
 def start
@@ -48,20 +68,18 @@ end
 def calculate(arr, stack = [])
   puts "Array: " + arr.to_s
   puts "Stack: " + stack.to_s
-  puts " "
+  puts
   operators = ["+", "-", "*", "/"]
 
   if arr.length == 3 && stack.empty?
+    puts "x"
     val1, val2, op = arr.shift, arr.shift, arr.shift
     result = perform_op(op, val1, val2)
-    puts "The result of the calculation is: " + ftoi(result)
+    puts "The result is: " + ftoi(result)
     return
-  elsif arr.length <= 1 && !stack.empty?
-    new_arr = []
-    stack.each_index{ |i| new_arr << stack[stack.length-1-i] }
-    new_arr += arr
-    calculate(new_arr, [])
+
   elsif operators.include?(arr.first)
+    puts "y"
     if stack.length < 2
       puts "Not enough arguments."
       exit
@@ -71,9 +89,20 @@ def calculate(arr, stack = [])
       stack.unshift(result)
       calculate(arr, stack)
     end
+
+  elsif arr.length <= 1 && !stack.empty?
+    puts "z"
+    new_arr = []
+    stack.each_index{ |i| new_arr << stack[stack.length-1-i] }
+    new_arr += arr
+    calculate(new_arr, [])
+
   elsif stack.length == 1 && arr.empty?
-    calculate(stack, arr)
+    puts "a"
+    puts "The result is: " + ftoi(stack.first)
+    exit
   else
+    puts "b"
     stack.unshift(arr.shift)
     calculate(arr, stack)
   end
